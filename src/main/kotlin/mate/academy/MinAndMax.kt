f8948fa01deeccd62af760dfa8a1f7bf6950167f
Oleksandr Kalenyk <carbonmail88@gmail.com>
2025-08-13 16:44:51 +0300

    fix

--- src/main/kotlin/mate/academy/MinAndMax.kt
+++ src/main/kotlin/mate/academy/MinAndMax.kt
@@ -3,7 +3,7 @@ package mate.academy
 fun getMinAndMax(inputArray: IntArray): IntArray {
     var min :Int = inputArray[0]
     var max :Int = inputArray[0]
-    for (i in 0 until inputArray.size - 1) {
+    for (i in 1 until inputArray.size) {
         if (inputArray[i] < min) {
             min = inputArray[i]
         }
